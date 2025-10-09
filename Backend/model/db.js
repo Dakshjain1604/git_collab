8705257588d71702f55267d0826164ea4b6f1577
Divyanshu agarwal <agarwaldivyanshu824@gmail.com>
2025-10-09 22:16:04 +0530

    cleaned

--- Backend/model/db.js
+++ Backend/model/db.js
@@ -1,55 +1,44 @@
-const mongoose=require("mongoose");
-const { string } = require("zod");
+const mongoose = require('mongoose');
 require('dotenv').config();
-mongoose.connect(process.env.MONGODB_URL)
+
+mongoose.connect(process.env.MONGODB_URL);
 
 
-
-const userSchema=new mongoose.Schema({
-    firstname:String,
-    lastname:String,
-    username:{type:String,unique:true},
-    password:{type:String}
-})
+const userSchema = new mongoose.Schema({
+  firstname: String,
+  lastname: String,
+  username: { type: String, unique: true },
+  password: { type: String }
+});
 
 const experienceSchema = new mongoose.Schema({
-    title : {type : String, required: true},
-    company : {type: String, required: true},
-    startdate : {type : Date, required: true},
-    enddate: {type : Date},
-    technologies_used : [{type: String}]
-})
+  title: { type: String, required: true },
+  company: { type: String, required: true },
+  startdate: { type: Date, required: true },
+  enddate: { type: Date },
+  technologies_used: [{ type: String }]
+});
 
 const educationSchema = new mongoose.Schema({
-    institution : {type: String},
-    year : {type : Date},
-    degree : {type : String}
-})
+  institution: { type: String },
+  year: { type: Date },
+  degree: { type: String }
+});
 
 const resumeSchema = new mongoose.Schema({
-    user_id: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    title : {type :String},
-    summary: {type: String},
-    education: [educationSchema],
-    experience : [experienceSchema],
-    projects : {type : String}
-})
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  title: { type: String },
+  summary: { type: String },
+  education: [educationSchema],
+  experience: [experienceSchema],
+  projects: { type: String }
+});
 
 
-const user=mongoose.model("User",userSchema);
-module.exports={
-    user
-}
-const resume = mongoose.model("Resume",resumeSchema);
-module.exports={
-    resume
-}
-const experience = mongoose.model("Experience",experienceSchema);
-module.exports={
-    experience
-}
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+const Experience = mongoose.models.Experience || mongoose.model('Experience', experienceSchema);
+const Education = mongoose.models.Education || mongoose.model('Education', educationSchema);
+const Resume = mongoose.models.Resume || mongoose.model('Resume', resumeSchema);
 
-const education = mongoose.model("Education",educationSchema);
-module.exports={
-    education
-}
+
+module.exports = { User, Experience, Education, Resume };
