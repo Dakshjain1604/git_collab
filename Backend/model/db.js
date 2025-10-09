195067148aa733c66b34b17d8d5d20c669d76d4c
Divyanshu agarwal <agarwaldivyanshu824@gmail.com>
2025-10-09 22:11:00 +0530

    schemas updated

--- Backend/model/db.js
+++ Backend/model/db.js
@@ -12,8 +12,22 @@ const userSchema=new mongoose.Schema({
     password:{type:String}
 })
 
+const experienceSchema = new mongoose.Schema({
+    title : {type : String, required: true},
+    company : {type: String, required: true},
+    startdate : {type : Date, required: true},
+    enddate: {type : Date},
+    technologies_used : [{type: String}]
+})
+
+const educationSchema = new mongoose.Schema({
+    institution : {type: String},
+    year : {type : Date},
+    degree : {type : String}
+})
 
 const resumeSchema = new mongoose.Schema({
+    user_id: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     title : {type :String},
     summary: {type: String},
     education: [educationSchema],
@@ -29,4 +43,13 @@ module.exports={
 const resume = mongoose.model("Resume",resumeSchema);
 module.exports={
     resume
+}
+const experience = mongoose.model("Experience",experienceSchema);
+module.exports={
+    experience
+}
+
+const education = mongoose.model("Education",educationSchema);
+module.exports={
+    education
 }
