4eba5e7c13e60307c581ecabe7763acf56c7ca51
vaibhvbhrdwj <vaibhavbhardwaj170904@gmail.com>
2025-10-12 20:39:45 +0530

    made a demo profile page and fetching data from backend api using axios and usecontext

--- Fe/src/Pages/Profile/Profile.jsx
+++ Fe/src/Pages/Profile/Profile.jsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import axios from 'axios'
+import { createContext , useContext ,useState } from 'react'
+
+const UserContext = createContext();
+
+
+
 
 const Profile = () => {
   return (
